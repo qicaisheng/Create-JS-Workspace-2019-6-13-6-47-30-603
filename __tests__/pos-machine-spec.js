032b095804374cea56be7bb2c22ad7815d94fc69
qicaisheng <qicaisheng@qq.com>
2020-04-03 18:35:34 +0800

    assemble product to orderItem

--- __tests__/pos-machine-spec.js
+++ __tests__/pos-machine-spec.js
@@ -1,7 +1,13 @@
 const {generateOrders, getProductBy} = require("../pos_machine")
 
 it('should generate orders', function () {
-    expect(generateOrders(['0001', '0003', '0005', '0003'])).toEqual([{"id": "0001", number: 1}, {"id": "0003", number: 2}, {"id": "0005", number: 1}])
+    expect(generateOrders(['0001', '0003', '0005', '0003'])).toEqual(
+        [
+            {"id": "0001", number: 1, product: {"id": "0001", "name" : "Coca Cola", "price": 3}}, 
+            {"id": "0003", number: 2, product: {"id": "0003", "name" : "Pepsi-Cola", "price": 5}}, 
+            {"id": "0005", number: 1, product: {"id": "0005", "name" : "Dr Pepper", "price": 7}}
+        ]
+    )
 });
 
 it('should get product by id', function () {
--- pos_machine.js
+++ pos_machine.js
@@ -4,14 +4,20 @@ const generateOrders = (barcodes) => {
     let orders = []
     barcodes.forEach(barcode => {
         let existedOrder = orders.find(order => order.id === barcode)
+        
         if (existedOrder) {
             existedOrder.number ++
         } else {
-            orders.push({"id": barcode, "number": 1})
+            orders.push(getOrderItem(barcode))
         }
     })
     return orders
 }
+
+getOrderItem = (id) => {
+    return {"id": id, "number": 1, product: getProductBy(id)};
+}
+
 
 const getProductBy = (id) => {
     let productsData = fs.readFileSync('products.json') 
